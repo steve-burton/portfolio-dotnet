f015aab0116079284c85f46f28338f4bd3c62aa0
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-03-03 08:13:35 -0800

    update Startup.cs and project.json

--- src/PortfolioDotnet/Controllers/ProjectsController.cs
+++ src/PortfolioDotnet/Controllers/ProjectsController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using PortfolioDotnet.Models;
+using PortfolioDotnet.Controllers;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
--- src/PortfolioDotnet/Startup.cs
+++ src/PortfolioDotnet/Startup.cs
@@ -6,31 +6,29 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using PortfolioDotnet.Controllers;
 
 namespace PortfolioDotnet
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        //public IConfigurationRoot Configuration { get; set; }
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        
+        public void Configure(IApplicationBuilder app)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
