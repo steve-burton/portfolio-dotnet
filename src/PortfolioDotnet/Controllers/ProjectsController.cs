8a06e9ce39ac16349376578486fd7eef0fccf43b
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-03-03 12:11:22 -0800

    add projects model and projects controller

--- src/PortfolioDotnet/Controllers/ProjectsController.cs
+++ src/PortfolioDotnet/Controllers/ProjectsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PortfolioDotnet.Controllers;
+using PortfolioDotnet.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,6 +16,12 @@ namespace PortfolioDotnet.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetProjects()
+        {
+            var starredProjects = Projects.GetProjects();
+            return View(starredProjects);
         }
     }
 }
---
+++ src/PortfolioDotnet/Models/Projects.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PortfolioDotnet.Models
+{
+    public class Projects
+    {
+        public static List<Projects> GetProjects()
+        {
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("/users/steve-burton/starred/?client_id=" + EnvironmentVariables.ClientId + "&client_secret=" + EnvironmentVariables.ClientSecret, Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.ClientId, EnvironmentVariables.ClientSecret);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var projectList = JsonConvert.DeserializeObject<List<Projects>>(jsonResponse["starred_url"].ToString());
+
+            return projectList;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => 
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
